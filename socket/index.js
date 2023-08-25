a8ecd1b9cc07d67163cb962964a8abc303053342
amine156a <135077247+amine156a@users.noreply.github.com>
2023-08-26 01:27:15 +0200

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -10,7 +10,10 @@ require("dotenv").config({
   path: "./.env",
 });
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000',],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
