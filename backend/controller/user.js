c1be5dc435660994c268a8d0aa4a4e8fc8f6364b
amine156a <135077247+amine156a@users.noreply.github.com>
2023-08-25 20:31:24 +0200

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://chezvous1-metv.vercel.app/activation/${activationToken}`;
+    const activationUrl = `http://chezvous1.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
